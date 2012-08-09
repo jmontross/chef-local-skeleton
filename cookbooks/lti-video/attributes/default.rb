0d46174a048c5090eec02f5f0bd7af07a115e34a
Joshua Montross <joshua.montross@gmail.com>
2012-08-09 10:32:51 -0700

    adds cookbook for lti-video /opentok

---
+++ cookbooks/lti-video/attributes/default.rb
@@ -0,0 +1,3 @@
+node['default']['lti']['video']['tokbox_api_key'] = 123
+node['default']['lti']['video']['tokbox_api_secret'] = 123
+node['default']['lti']['video']['tokbox_api_server'] = "staging.tokbox.com/h1"
