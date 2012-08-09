c4b73dc76013f38e8dcd8d4a3014bd69d68ac311
Joshua Montross <joshua.montross@gmail.com>
2012-08-09 11:48:12 -0700

    fixes default attrs

--- cookbooks/lti-video/attributes/default.rb
+++ cookbooks/lti-video/attributes/default.rb
@@ -1,3 +1,3 @@
-node['default']['lti']['video']['tokbox_api_key'] = 123
-node['default']['lti']['video']['tokbox_api_secret'] = 123
-node['default']['lti']['video']['tokbox_api_server'] = "staging.tokbox.com/h1"
+default["lti"]["video"]["tokbox_api_key"] = "123"
+default["lti"]["video"]["tokbox_api_secret"] = "123"
+default["lti"]["video"]["tokbox_api_server"] = "staging.tokbox.com/h1"
